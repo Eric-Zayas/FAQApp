52f09bc0ba66d0d9bcb1327eba83c758d3c559da
Eric Zayas <ezayas16@icloud.com>
2017-09-03 15:59:33 -0400

    adds to local storage

--- faqapp/src/app/services/data.service.ts
+++ faqapp/src/app/services/data.service.ts
@@ -6,34 +6,52 @@ export class DataService {
   questions:Question[];
 
   constructor() {
-    this.questions = [
-      {
-        text: 'What is your name?',
-        answer: 'I am Genji',
-        hide: true,
-      },
-      {
-        text: 'What is your favorite color?',
-        answer: 'My favorite color is silver',
-        hide: true,
-      },
-      {
-        text: 'What is your favorite language?',
-        answer: 'My favorite language is japanese, I love saying: Ryujin no ken wo kurae!',
-        hide: true,
-      },
-      {
-        text: 'What do you love?',
-        answer: 'I love Overwatch',
-        hide: true,
-      }
-    ]
+    // this.questions = [
+    //   {
+    //     text: 'What is your name?',
+    //     answer: 'I am Genji',
+    //     hide: true,
+    //   },
+    //   {
+    //     text: 'What is your favorite color?',
+    //     answer: 'My favorite color is silver',
+    //     hide: true,
+    //   },
+    //   {
+    //     text: 'What is your favorite language?',
+    //     answer: 'My favorite language is japanese, I love saying: Ryujin no ken wo kurae!',
+    //     hide: true,
+    //   },
+    //   {
+    //     text: 'What do you love?',
+    //     answer: 'I love Overwatch',
+    //     hide: true,
+    //   }
+    // ]
    }
+   //getQuestions from local Storage
    getQuestions(){
+
+    if(localStorage.getItem('questions') === null){
+      this.questions = [];
+    }else{
+      this.questions = JSON.parse(localStorage.getItem('questions'));
+    }
      return this.questions;
    }
    addQuestion(question:Question){
-     this.questions.unshift(question);
+    this.questions.unshift(question);
+     let questions;
+    if(localStorage.getItem('questions') === null){
+      questions = [];
+      this.questions.unshift(question);
+      JSON.stringify(localStorage.setItem('questions',questions))
+    }else{
+      questions = JSON.parse(localStorage.getitem('questions'));
+      questions.unshift(question);
+      localStorage.setItem('questions',JSON.stringify(questions));
+    }
+   
    }
 
    removeQuestion(question){
